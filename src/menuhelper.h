89d6d2b2bfc472ffce1501bb2b894da688c04ff6
Travis Bortz <cromo_man94@yahoo.com>
2016-05-19 19:00:17 -0500

    Code organization of menus.c menus.h
    Moved the app-specific functions (most of them) into the menuhelper.c and menuhelper.h functions, which removed the need to use #include "game.h" from menus.c

---
+++ src/menuhelper.h
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "menus.h"
+#include "game.h"
+#pragma once
+
+//prototypes
+void layerzeroupdate(MenuData* menudata, GameData* gamedata);
+void layeroneupdate(MenuData* menudata, GameData* gamedata);
+void layertwoupdate(MenuData* menudata, GameData* gamedata);
+void layerthreeupdate(MenuData* menudata, GameData* gamedata);
+void layerfourupdate(MenuData* menudata, GameData* gamedata);
+void layerfiveupdate(MenuData* menudata, GameData* gamedata);
+
